02dbc359414d615808e4087bb08bb7b446da5a3b
chrizantona <sharukondondon@gmail.com>
2024-11-24 21:27:13 +0300

    Integrate Postgres and implement versioned storage logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    _ "github.com/lib/pq"
     "go-course-2024/internal/pkg/server"
     "go-course-2024/internal/pkg/storage"
     "log"
@@ -8,9 +9,20 @@ import (
     "os/signal"
     "syscall"
     "time"
+    "database/sql"
+    "fmt"
 )
 
 func main() {
+
+
+    db, err := setupDatabase()
+	if err != nil {
+		log.Fatalf("Database setup failed: %v", err)
+	}
+	defer db.Close()
+
+
     myStorage := storage.NewStorage()
     const storageFilePath = "/root/storage_data/storage.json"
 
@@ -32,13 +44,33 @@ func main() {
 
     stop := make(chan os.Signal, 1)
     signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
-
     <-stop
     log.Println("Shutting down gracefully...")
-
     if err := myStorage.SaveToFile(storageFilePath); err != nil {
         log.Printf("Failed to save storage: %v", err)
     } else {
         log.Println("Storage saved successfully")
     }
+}
+
+
+func setupDatabase() (*sql.DB, error) {
+	dsn := os.Getenv("POSTGRES") 
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	query := `
+	CREATE TABLE IF NOT EXISTS core (
+		version bigserial PRIMARY KEY,
+		timestamp bigint NOT NULL,
+		payload JSONB NOT NULL
+	)`
+
+	if _, err := db.Exec(query); err != nil {
+		return nil, fmt.Errorf("failed to create table: %w", err)
+	}
+
+	return db, nil
 }
